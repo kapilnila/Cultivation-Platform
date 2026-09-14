606edc681570e6a01f93d52b460aea3363610e94
Kapil Nila <nilakapil1@gmail.com>
2026-09-15 02:18:59 +0530

    Initialize Celery app for Django project

---
+++ celery.py
@@ -0,0 +1,18 @@
+import os
+
+from celery import Celery
+
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE",
+    "Cultivation-Platform.settings",
+)
+
+app = Celery("Cultivation-Platform")
+
+app.config_from_object(
+    "django.conf:settings",
+    namespace="CELERY",
+)
+
+app.autodiscover_tasks()
